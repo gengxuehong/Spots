3d57a2aff40a9cc7b311026b6bb3ad79470f00f9
gengxuehong <gengxuehong@gmail.com>
2011-05-19 01:56:47 +0800

    Add function to generate DataTable from class. Not tested yet.
    Signed-off-by: gengxuehong <gengxuehong@gmail.com>

--- Projects/EDoorSolution/src/edoor/Data/DataColumn.java
+++ Projects/EDoorSolution/src/edoor/Data/DataColumn.java
@@ -1,5 +1,55 @@
 package edoor.Data;
 
+/**
+ * Column in the table
+ * @author Geng Xuehong
+ */
 public class DataColumn {
-
+    private String _name = "";
+    private Class<?> _type = null;
+    private int _maxlen = -1;
+    private DataTable _table = null;
+    
+    /**
+     * Construct a emtpy column
+     */
+    protected DataColumn(DataTable owner) {
+        _table = owner;
+    }
+    
+    /**
+     * Construct a column with properties
+     * @param name Column name
+     * @param type Data type
+     * @param maxlen Max length of column
+     */
+    protected DataColumn(DataTable owner, String name, Class<?> type, int maxlen) {
+        _name = name;
+        _type = type;
+        _maxlen = maxlen;
+    }
+    
+    public void setName(String name) {
+        _name = name;
+    }
+    
+    public String getName() {
+        return _name;
+    }
+    
+    public void setType(Class<?> type) {
+        _type = type;
+    }
+    
+    public Class<?> getType() {
+        return _type;
+    }
+    
+    public void setMaxLen(int maxLen) {
+        _maxlen = maxLen;
+    }
+    
+    public int getMaxLen() {
+        return _maxlen;
+    }
 }
--- Projects/EDoorSolution/src/edoor/Data/DataTable.java
+++ Projects/EDoorSolution/src/edoor/Data/DataTable.java
@@ -1,10 +1,116 @@
 package edoor.Data;
 
+import java.lang.reflect.Field;
+import java.util.*;
+
 /**
  * class for access data stored in a table
  * @author Geng Xuehong
  *
  */
 public class DataTable {
-
+    private String _name = "";
+    private LinkedHashMap<String, DataColumn> _columns = new LinkedHashMap<String, DataColumn>();
+    private LinkedList<DataRow> _rows = new LinkedList<DataRow>();
+    
+    /**
+     * Generate DataTable from a Class
+     * @param cls Class used to create data table
+     * @return Generated data table
+     */
+    public static DataTable fromClass(Class<?> cls) throws DataException {
+        DataRecord annoRecord = (DataRecord)cls.getAnnotation(DataRecord.class);
+        if(annoRecord == null) {
+            throw new DataException("Class does not marked as DataRecord!");
+        }
+        String tableName = annoRecord.TableName();
+        
+        DataTable table = new DataTable(tableName);
+        Field[] fields = cls.getFields();
+        for(Field field : fields) {
+            DataField annoField = (DataField)field.getAnnotation(DataField.class);
+            if(annoField == null) continue;
+            String fieldName = annoField.FieldName();
+            Class<?> type = field.getClass();
+            int maxlen = annoField.MaxLength();
+            table.NewColumn(fieldName, type, maxlen);
+        }
+        
+        return table;
+    }
+    
+    public DataTable() {
+        
+    }
+    
+    public DataTable(String name) {
+        _name = name;
+    }
+    
+    public void setName(String name) {
+        _name = name;
+    }
+    public String getName() {
+        return _name;
+    }
+    
+    /**
+     * Get collection of columns
+     * @return a collection that contains all columns
+     */
+    public Collection<DataColumn> getColumns() {
+        return _columns.values();
+    }
+    
+    /**
+     * Create a new column in table
+     * @param name Name of new column
+     * @param type Data type of new column
+     * @param maxlen Max length of column. Ignored for fix size columns
+     * @return Newly created column
+     * @throws DataException 
+     */
+    public DataColumn NewColumn(String name, Class<?> type, int maxlen) throws DataException {
+        if(_columns.containsKey(name)) {
+            throw new DataException("Column already exist!");
+        }
+        
+        DataColumn col = new DataColumn(this, name, type, maxlen);
+        _columns.put(name, col);
+        
+        return col;
+    }
+    
+    /**
+     * Remove column from table
+     * @param name Name of column to be removed
+     * @throws DataException
+     */
+    public void RemoveColumn(String name) throws DataException {
+        if(!_columns.containsKey(name))
+            throw new DataException("Column not exist!");
+        _columns.remove(name);
+    }
+    
+    /**
+     * Create a new row base on this table's schema. 
+     * New row could only be append to the same table.
+     * @return Newly created data row
+     * @throws DataException 
+     */
+    public DataRow NewRow() throws DataException {
+        DataRow row = new DataRow();
+        // TODO Implement NewRow
+        return row;
+    }
+    
+    /**
+     * Append newly created row to this table
+     * @param row Row to be appended
+     * @throws DataException 
+     */
+    public void AppendRow(DataRow row) throws DataException {
+        // TODO Implement AppendRow
+    }
+    
 }
